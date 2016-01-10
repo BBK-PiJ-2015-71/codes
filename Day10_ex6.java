0bc3f54657c6b79a0547b98044ca32b664119399
BBK-PiJ-2015-71 <ullash.hazarika@gmail.com>
2016-01-10 16:43:58 +0000

    Day 10, Exercise 6
    Day 10, Exercise 6 - subclass Lecturer needed a constructor with an
    argument of Type String for name, for the superclass constructor which
    has a String variable name as an argument.

---
+++ Day10_ex6.java
@@ -0,0 +1,11 @@
+public class Day10_ex6
+{
+	public static void main(String[] str)
+	{
+		Lecturer adam = new Lecturer("Adam Smith");
+		System.out.println(adam.getName());
+		adam.teach("JAva");
+		adam.doResearch("Genetics");
+	}
+
+}
