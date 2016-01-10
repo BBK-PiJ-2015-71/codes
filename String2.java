d1e0a83d3542c99cd8da9b14a1d28370cd3604cc
BBK-PiJ-2015-71 <ullash.hazarika@gmail.com>
2016-01-10 16:48:17 +0000

    Day 10, exercise 7
    Day 10, exercise 7 - Since class String2 tries to extend final class
    String, this gives a compile error

---
+++ String2.java
@@ -0,0 +1,13 @@
+/**
+*String class cannot be extended as it is final. This class gives compile error.
+*/
+
+public class String2 extends String
+{
+	public void printEven()
+	{
+		System.out.println("Test");
+	}
+	
+
+}
