43a93caadfedbc0dd499868209217e6f32cca588
BBK-PiJ-2015-71 <ullash.hazarika@gmail.com>
2016-01-10 15:37:55 +0000

    Day 10, Exercise 4.2
    Day 10, Exercise 4.2 - Reducing visibility by overriding a method with a
    method with same signature but empty

---
+++ RestrictedSmartPhone.java
@@ -0,0 +1,14 @@
+public class RestrictedSmartPhone extends SmartPhone
+{
+	public RestrictedSmartPhone(String brand) 
+	{
+		super(brand);
+	}
+
+	@Override
+	public void playGame(String game)
+	{
+		
+	}
+
+}
